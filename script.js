833e8f4e4bef19fe6607afcbe7d8340e43780f2e
FrancisGil-dev <130513312+Kipiww@users.noreply.github.com>
2023-11-14 15:23:48 +0800

    committed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// JavaScript to toggle menu visibility
-document.querySelector('[data-collapse-toggle="navbar-sticky"]').addEventListener('click', function() {
+
+//Toggle Hamburger icon
+const toggleNav = () => {
+    // JavaScript using classList.toggle() to toggle menu visibility
+    document.querySelector('[data-collapse-toggle="navbar-sticky"]').addEventListener('click', function() {
     var navbar = document.getElementById('navbar-sticky');
-    if (navbar.style.display === 'none') {
-      navbar.style.display = 'block';
-    } else {
-      navbar.style.display = 'none';
-    }
+    navbar.classList.toggle('hidden'); // Toggle the 'hidden' class
   });
+}
   
