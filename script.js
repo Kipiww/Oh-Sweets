29c09ebbe1ce5343f54ec7d9f5eb9a3450f192d2
FrancisGil-dev <130513312+Kipiww@users.noreply.github.com>
2023-11-14 15:14:42 +0800

    commit

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-
-//Function to click navbar to scroll
-const clickToScroll = () =>{
- //buttons
-const navLinks = document.querySelectorAll('nav ul li');
- //sections
-    const sections = document.querySelectorAll('section');
- //navButtons
-  const home = navLinks[0];
-  const about = navLinks[1];
-  const menu = navLinks[2];
-  const contact = navLinks[3];
-
- //Function to scroll to view sections
-    const clickScroll = (sec) => {sec.scrollIntoView({behaviour: 'smooth'})}
-    
-    about.addEventListener('click',() => {clickScroll(sections[0])})
-    menu.addEventListener('click',() => {clickScroll(sections[1])})
-    contact.addEventListener('click',() => {clickScroll(sections[2])})
-}
-clickToScroll();
-
-
-
-
-
-
-
-
-
+// JavaScript to toggle menu visibility
+document.querySelector('[data-collapse-toggle="navbar-sticky"]').addEventListener('click', function() {
+    var navbar = document.getElementById('navbar-sticky');
+    if (navbar.style.display === 'none') {
+      navbar.style.display = 'block';
+    } else {
+      navbar.style.display = 'none';
+    }
+  });
+  
